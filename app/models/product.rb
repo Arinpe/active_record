d96938246e51fbfc2f4074795817398f0bc68cd9
Arinpe <dehtolah@gmail.com>
2021-05-20 15:19:43 +0100

    Work on validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
-        validates :email,
-        format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-                  uniqueness: { case_sensitive: false },
-                  length: { minimum: 4, maximum: 254 }
 end
+
+# validates :email,
+# format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
+#           uniqueness: { case_sensitive: false },
+#           length: { minimum: 4, maximum: 254 }
