9d6162c222844f78b8b662f1e7b4168dd20cd1af
Arinpe <dehtolah@gmail.com>
2021-05-21 13:37:58 +0100

    Create a new validation

--- app/models/product.rb
+++
@@ -1,7 +0,0 @@
-class Product < ApplicationRecord
-end
-
-# validates :email,
-# format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
-#           uniqueness: { case_sensitive: false },
-#           length: { minimum: 4, maximum: 254 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_113121) do
+ActiveRecord::Schema.define(version: 2021_05_21_123021) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_05_20_113121) do
     t.string "name"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
