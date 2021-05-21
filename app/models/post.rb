3e79714737ce71fa41f7cd2a88520094704d79c4
Deepakdanger <depk.96.kumar@gmail.com>
2021-05-21 18:32:40 +0530

    Add validations of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     validates :user_id, presence: true
+    validates :post1, length: { minimum: 8,maximum: 100,
+        too_short: "must have at least %{count} words",
+        too_long: "%{count} characters is the maximum allowed" }
 end
