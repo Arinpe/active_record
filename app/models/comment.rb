a66a30c7f74486838f5d8610d7e9283a7c0751c5
Deepakdanger <depk.96.kumar@gmail.com>
2021-05-24 14:58:17 +0530

    Remove rubocop errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :comment, length: { minimum: 3, maximum: 100,
-                                too_short: 'must have at least %{count} words',
-                                too_long: '%{count} characters is the maximum allowed' }
+                                too_short: 'must have at least %<count>s words',
+                                too_long: '%<count>s characters is the maximum allowed' }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
   has_many :comment
   validates :user_id, presence: true
   validates :post1, length: { minimum: 8, maximum: 100,
-                              too_short: 'must have at least %{count} words',
-                              too_long: '%{count} characters is the maximum allowed' }
+                              too_short: 'must have at least %<count>s words',
+                              too_long: '%<count>s characters is the maximum allowed' }
 end
